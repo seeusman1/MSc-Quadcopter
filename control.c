a6a69bb18becfa893c3af0276d382b9d1cf5ef9c
Usman <seeusman1@gmail.com>
2017-09-29 12:04:19 +0200

    manual and yaw control

--- control.c
+++ control.c
@@ -13,27 +13,37 @@
 #include "in4073.h"
 #include "statemanager/statemanager.h"
 
-#define MAX_SETPOINT 700
+#define MAX_SETPOINT 800
+#define MIN_SETPOINT 100 // needs to check at what value rotor starts spinning
+
+
 void update_motors(void)
 {					
-
 	if (ae[0] > MAX_SETPOINT) {
 		motor[0] = MAX_SETPOINT;
+	} else if (ae[0]< MIN_SETPOINT) {
+		motor[0] = 0;
 	} else {
 		motor[0] = ae[0];
 	}
 	if (ae[1] > MAX_SETPOINT) {
 		motor[1] = MAX_SETPOINT;
+	} else if (ae[1]< MIN_SETPOINT) {
+		motor[1] = 0;
 	} else {
 		motor[1] = ae[1];
 	}
 	if (ae[2] > MAX_SETPOINT) {
 		motor[2] = MAX_SETPOINT;
+	} else if (ae[2]< MIN_SETPOINT) {
+		motor[2] = 0;
 	} else {
 		motor[2] = ae[2];
 	}
 	if (ae[3] > MAX_SETPOINT) {
 		motor[3] = MAX_SETPOINT;
+	} else if (ae[3]< MIN_SETPOINT) {
+		motor[3] = 0;
 	} else {
 		motor[3] = ae[3];
 	}
@@ -43,7 +53,7 @@ void update_motors(void)
  * Author: Rutger van den Berg
  * Control step for manual mode. 
  */
-void manual() {
+/* void manual() {
 	// int16_t B = 1;
 	// int16_t D = 1;
 	int32_t Z = current_pose.lift;
@@ -54,7 +64,77 @@ void manual() {
 	ae[1] = (-Z/4 -L/2 + N/4 + INT16_MAX) /64;
 	ae[2] = (-Z/4 - M/2 - N/4 + INT16_MAX) /64;
 	ae[3] = (-Z/4 + L/2 + N/4 + INT16_MAX) /64;
+} */
+
+
+ void manual() {
+	int32_t Z = (current_pose.lift + INT16_MAX)/64;
+	int32_t L = (current_pose.roll)/64;
+	int32_t M = (current_pose.pitch)/64;
+	int32_t N = (current_pose.yaw)/64;
+	
+	if (Z >= MIN_SETPOINT){
+	
+		ae[0] = -Z + M/2 - N/4;
+		ae[1] = -Z -L/2 + N/4;
+		ae[2] = -Z - M/2 - N/4;
+		ae[3] = -Z + L/2 + N/4;
+	}else {
+		ae[0] = 0;
+		ae[1] = 0;
+		ae[2] = 0;
+		ae[3] = 0;
+	}
+
+	//ae[0] = Z;
+	//ae[1] = L;
+	//ae[2] = M;
+	//ae[3] = N;
+
+
 }
+
+void yaw_control() {
+	int32_t Z = (current_pose.lift + INT16_MAX)/64;
+	int32_t L = (current_pose.roll)/64;
+	int32_t M = (current_pose.pitch)/64;
+	int32_t N = (current_pose.yaw)/64;
+	
+	int32_t P =4;
+	int32_t A = 512 / INT16_MAX ; 
+	int32_t N_new = P * (N-(A * sr));// A is amplification factor i.e. A*sr = N 
+	
+	ae[0] = -Z + M/2 - N_new/4;
+	ae[1] = -Z -L/2 + N_new/4;
+	ae[2] = -Z - M/2 - N_new/4;
+	ae[3] = -Z + L/2 + N_new/4;
+		
+
+}
+
+
+//needs modificaitons
+void full_control() {
+	int32_t Z = (current_pose.lift + INT16_MAX)/64;
+	int32_t L = (current_pose.roll)/64;
+	int32_t M = (current_pose.pitch)/64;
+	int32_t N = (current_pose.yaw)/64;
+	
+	uint8_t P =4;
+	uint8_t A =4;
+	int32_t N_new = P * (N-(A * sr));// A is amplification factor i.e. A*sr = N 
+	
+	ae[0] = -Z/2 + M/2 - N_new/4;
+	ae[1] = -Z/2 -L/2 + N_new/4;
+	ae[2] = -Z/2 - M/2 - N_new/4;
+	ae[3] = -Z/2 + L/2 + N_new/4;
+		
+
+}
+
+
+
+
 
 void panic() {
 	ae[0] = 0;
